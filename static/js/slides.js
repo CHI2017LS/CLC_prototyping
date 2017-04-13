a52b546e67869729ab81ce0b77c043fec6369357
Yu-Hsuan <yu-hsuan@Yu-Hsuande-MacBook-Pro.local>
2017-04-13 14:46:30 +0800

    fix bug of showing the first sample slide

--- static/js/slides.js
+++ static/js/slides.js
@@ -49,8 +49,11 @@ function addSlide(id, img_url) {
     $(newSlide).attr('id', 'slide' + id);
     $(newSlide).find('a').attr('onclick', "changePad('" + sessionID + sessionTitle + id + "')");
     $(newSlide).find('img.img-responsive').attr('src', img_url);
+    $(newSlide).find('img.img-responsive').css("display", "block");
     $(newSlide).find('p.number-of-editing').attr('id', 'padInfo-' + sessionID + sessionTitle + id);
+    $(newSlide).find('p.number-of-editing').css("display", "block");
     $(newSlide).find('p.keyword').attr('id', 'padKeyword-' + sessionID + sessionTitle + id);
+    $(newSlide).find('p.keyword').css("display", "block");
     //var el = $("<li class='list-group-item'><b><img src=" +  img_url + ":</b> " + "ee" + "</li>");//modify
     slideList.append(newSlide);
 }
