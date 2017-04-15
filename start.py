914eea9b4b0b8d93bc13c9b881393e1a4f653120
Yu-Hsuan <yu-hsuan@Yu-Hsuande-MBP.Home>
2017-04-15 18:58:06 +0800

    change server location

--- start.py
+++ start.py
@@ -85,5 +85,5 @@ def test_disconnect():
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
     # app.run()
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0')
 
