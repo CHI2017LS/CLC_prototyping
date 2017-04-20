035ca9a16ed53441682019437ce36423892d27e6
Yu-Hsuan <yu-hsuan@Yu-Hsuande-MacBook-Pro.local>
2017-04-20 14:57:59 +0800

    remove socket in start.py

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, render_template, request
 from etherpad_lite import EtherpadLiteClient
-from flask_socketio import SocketIO, emit	# pip install flask-socketio
 from watson_developer_cloud import AuthorizationV1
 from watson_developer_cloud import SpeechToTextV1
 from OpenSSL import SSL
@@ -18,26 +17,6 @@ app.config.update(
 	TEMPLATES_AUTO_RELOAD = True
 )
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-thread = None
-
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    while True:
-        socketio.sleep(3)
-        data = getPadUsersCount()
-        socketio.emit('response',
-                      {'data': data}, namespace='/getpadusercount')
-
-def getPadUsersCount():
-	c = EtherpadLiteClient(base_params={'apikey':'8b370ace91baa8557c685d75d70a6c2005e19761a5cf55a83611c3773d3d4c38'})
-	padList = c.listAllPads()
-	userCount = {}
-
-	for ID in padList['padIDs']:
-		userCount[ID] = c.padUsersCount(padID=ID)['padUsersCount']
-	
-	return json.dumps(userCount)
 
 @app.route("/")
 def index():
@@ -86,23 +65,12 @@ def pasteText(padID=None,text=None):
 	message = c.appendText(padID=padID, text=text)
 	return json.dumps(message)
 
-@socketio.on('connect', namespace='/getpadusercount')
-def test_connect():
-	global thread
-	if thread is None:
-		thread = socketio.start_background_task(target=background_thread)
-#	emit('response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/getpadusercount')
-def test_disconnect():
-    print('Client disconnected', request.sid)
- #   emit('response', {'data': 'Disconnected'})
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
     # app.run()
     # socketio.run(app, debug=True, host='0.0.0.0')
     context = (cer, key)
-    # socketio.run(app, host='0.0.0.0', port=5000, debug = True, ssl_context=context)
-    socketio.run(app, host='0.0.0.0', port=5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug = True, ssl_context=context)
+    # app.run(host='0.0.0.0', port=5000, debug = True)
 
