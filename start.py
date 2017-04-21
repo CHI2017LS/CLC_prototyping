6ea70242b62363366aeb49837d5b2736ae4a70eb
Yu-Hsuan <yu-hsuan@Yu-Hsuande-MacBook-Pro.local>
2017-04-21 12:46:23 +0800

    change homepage to menu.html

--- start.py
+++ start.py
@@ -20,9 +20,6 @@ app.config['SECRET_KEY'] = 'secret!'
 
 @app.route("/")
 def index():
-	return render_template('index.html')
-@app.route("/menu")
-def menu():
 	return render_template('menu.html')
 
 @app.route("/slides")
